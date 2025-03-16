b1951bbcc23c6a34de07f656be292d03488c6430
ankit-tamrakar <anki.139@gmail.com>
2025-03-16 01:32:22 -0700

    json conifg for email

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import json
+
 from trigger_email_service import load_data, send_email
 
 from utils.logger import setlog
@@ -8,15 +10,16 @@ log = setlog("main")
 if __name__ == '__main__':
     cust_data = load_data()
     try:
-        with open("src/config/introduction_email.txt", "r") as email_body:
-            body = email_body.read()
+        with open("src/config/introduction_email.json", "r") as email_details:
+            email = json.load(email_details)
     except FileNotFoundError as file_error:
-        log.critical(f"File not found at src/config/introduction_email.txt. Exception caught - {file_error}")
+        log.critical(f"File not found at src/config/introduction_email.json. Exception caught - {file_error}")
         exit(1)
 
     for _, customer in cust_data.iterrows():
         send_email(
             customer['email'],
-            "Introduction to Kavamoss Natural Products",
-            body.format(reciever=customer['greeting_alias'])
+            email["subject"],
+            email["body"].format(reciever=customer['greeting_alias']),
+            attachment_paths=["data/input/seller_export/Supermarkets.txt"]
         )
